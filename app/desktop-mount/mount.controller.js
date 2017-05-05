6a041f98c80d52a8221a91c09d26e466f01d0666
Stefan Dimitrov <stefan@dimitrov.li>
2017-05-05 06:46:10 +0000

    Execute Windows commands.

--- app/desktop-mount/mount.controller.js
+++ app/desktop-mount/mount.controller.js
@@ -1,4 +1,10 @@
 /*globals angular */
 angular.module("bkApp").controller('mountController', ['$scope', '$window', '$routeParams', '$firebaseObject', 'userService', 'projectsService', function($scope, $window, $routeParams, $firebaseObject, userService, projectsService) {
-
+	$scope.mount = function() {
+		// call the function
+execute('openvpn.exe', function(output, error) {
+	console.log(error);
+    console.log(output);
+});
+	}
 }]);
